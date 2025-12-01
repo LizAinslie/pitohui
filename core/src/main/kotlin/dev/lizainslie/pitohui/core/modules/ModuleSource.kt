73d1a5093399dadf87764723d76475538e3a2a24
Liz Ainslie <lizainslie16@gmail.com>
2025-12-01 11:23:20 -0700

    doc: document module system enums

--- core/src/main/kotlin/dev/lizainslie/pitohui/core/modules/ModuleSource.kt
+++ core/src/main/kotlin/dev/lizainslie/pitohui/core/modules/ModuleSource.kt
@@ -1,6 +1,20 @@
 package dev.lizainslie.pitohui.core.modules
 
+/**
+ * ModuleSource represents the origin of a module, indicating whether it is
+ * built-in or added externally through jar loading.
+ */
 enum class ModuleSource {
+    /**
+     * Internal modules are built into the bot and are typically part of its core
+     * functionality.
+     */
     INTERNAL,
+
+    /**
+     * External modules are added to the bot through jar loading, allowing for
+     * additional features and functionalities to be integrated and hot loaded at
+     * runtime.
+     */
     EXTERNAL
 }
