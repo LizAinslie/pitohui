40f3baf5d47a734f5f2335c30bc9543f7609c228
Mey <mey@lizainslie.dev>
2025-12-26 23:23:46 -0500

    feat: text argument parsing foundation

--- core/src/main/kotlin/dev/lizainslie/pitohui/core/commands/argument/text/TextArguments.kt
+++ core/src/main/kotlin/dev/lizainslie/pitohui/core/commands/argument/text/TextArguments.kt
@@ -1,10 +1,12 @@
-package dev.lizainslie.pitohui.core.commands.argument
+package dev.lizainslie.pitohui.core.commands.argument.text
 
 import dev.lizainslie.pitohui.core.commands.BaseCommand
 
 class TextArguments(private val command: BaseCommand, raw: String) {
     init {
-
+        // todo: parse arguments into a map
+        // todo: validate arguments against command definition
+        // todo: store arguments in a way that they can be retrieved by name and type
     }
 
     companion object {
