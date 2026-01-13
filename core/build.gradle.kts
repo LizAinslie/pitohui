e8816763ea88ba48ae0b884db330aa698c4f7b0b
Mey <mey@lizainslie.dev>
2026-01-13 14:02:59 -0600

    feat: add license & publishing config for core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,7 +4,10 @@ plugins {
     id("buildsrc.convention.kotlin-jvm")
     // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
     alias(libs.plugins.kotlinPluginSerialization)
+    alias(libs.plugins.mavenPublish)
 }
+
+version = "0.0.1"
 
 dependencies {
     implementation(libs.bundles.exposed)
@@ -13,5 +16,52 @@ dependencies {
     implementation(libs.slf4jApi)
     implementation(libs.bundles.okhttp)
     implementation(libs.okio)
+
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "lizAinslie"
+
+            url = uri(
+                if (version.toString().endsWith("SNAPSHOT"))
+                    "https://repo.lizainslie.dev/repository/maven-snapshots/"
+                else "https://repo.lizainslie.dev/repository/maven-releases/"
+            )
+
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    mavenPublishing {
+        coordinates("dev.lizainslie.pitohui", "pitohui-core", version.toString())
+
+        pom {
+            name.set("Pitohui Core")
+            description.set("Core functionality for Pitohui, a multi-platform bot framework written in Kotlin")
+            inceptionYear.set("2025")
+            url.set("https://git.lizainslie.dev/crack-cafe/pitohui/")
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("https://opensource.org/license/mit")
+                    distribution.set("https://opensource.org/license/mit")
+                }
+            }
+            developers {
+                developer {
+                    id.set("mey")
+                    name.set("Mey Ainslie")
+                    url.set("https://git.lizainslie.dev/mey/")
+                }
+            }
+            scm {
+                url.set("https://github.com/username/mylibrary/")
+                connection.set("scm:git:git://git.lizainslie.dev/crack-cafe/pitohui.git")
+                developerConnection.set("scm:git:ssh://git@gitl.izainslie.dev/crack-cafe/pitohui.git")
+            }
+        }
+    }
 }
